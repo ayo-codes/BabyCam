89fb9de2c6986e458275e0fe25896ce651b47a99
Ayo Onamusi <onamusia@gmail.com>
2022-12-22 23:38:45 +0000

    added motion detection

--- babycam.py
+++ babycam.py
@@ -2,6 +2,7 @@ import BlynkLib # imports the blynk library which helps us connect to the mobile
 from sense_hat import SenseHat # imports the library to enable use sensehat features
 from time import sleep #imports time functions
 from dotenv import dotenv_values #imports .env values 
+import RPi.GPIO as GPIO #allows us to use the GPIO pins 
 
 #loads MQTT configuration values from .env file
 config = dotenv_values(".env")
@@ -13,8 +14,19 @@ blynk = BlynkLib.Blynk(config['BLYNK_AUTH'])
 sense = SenseHat()
 sense.clear()
 
+#motion sensor 
+motionPin = 21 #GPIO pin 21 
+GPIO.setmode(GPIO.BCM) # sets the GPIO to BCM numbering system 
+GPIO.setup(motionPin,GPIO.IN) # sets GPIO 21 as the input pin 
+
 #Runs the connection between blynk and raspberry pi 
-while True:
-  blynk.run()
-  blynk.virtual_write(4,round(sense.humidity,2)) #writes to virtual pin 4 and sends data from the senseHAT for humidity
-  sleep(.5)
+try:
+  while True:
+    blynk.run()
+    blynk.virtual_write(4,round(sense.humidity,2)) #writes to virtual pin 4 and sends data from the senseHAT for humidity
+    motion = GPIO.input(motionPin) # set the variable motion as the input of the motionPin 
+    blynk.virtual_write(5,motion) # writes to virtual pin 5 and data sent is the value of the variable motion
+    sleep(.5)
+except KeyboardInterrupt:
+  GPIO.cleanup()
+  print('GPIO Good to Go')
