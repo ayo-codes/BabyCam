d3ac73c0994745adde31a5f72205f954823730e2
Ayo Onamusi <onamusia@gmail.com>
2022-12-22 23:02:31 +0000

    connect to blynk

---
+++ babycam.py
@@ -0,0 +1,18 @@
+import BlynkLib # imports the blynk library which helps us connect to the mobile app
+from sense_hat import SenseHat # imports the library to enable use sensehat features
+from time import sleep #imports time functions
+from dotenv import dotenv_values #imports .env values 
+
+#loads MQTT configuration values from .env file
+config = dotenv_values(".env")
+
+#initialise Blynk , Put in your own keys into blynk_auth in .env 
+blynk = BlynkLib.Blynk(config['BLYNK_AUTH'])
+
+#initialise SenseHAT 
+sense = SenseHat()
+sense.clear()
+
+#Runs the connection between blynk and raspberry pi 
+while True:
+  blynk.run()
